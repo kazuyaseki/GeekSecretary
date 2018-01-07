d0e1ecbabfa9223bab68db370641df06f96698b3
kazuya-seki <track613armory@gmail.com>
2018-01-07 13:17:52 +0900

    フッターのストーリーを追加

---
+++ src/components/stories/Footer.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { storiesOf, action } from "@storybook/react";
+
+storiesOf("Footer", module).add("default", () => (
+  <footer>
+    <a href="https://github.com/kazuyaseki/GeekSecretary">Github</a>
+  </footer>
+));
